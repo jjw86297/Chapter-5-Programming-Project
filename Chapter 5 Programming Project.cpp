f7b327cd5f367a182086dc028ed4382a7df5de19
jjw86297 <jjw86297@email.vccs.edu>
2024-10-24 22:25:11 -0400

    First (and hopefully Final) Commit

--- Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project.cpp
@@ -4,10 +4,46 @@
 //master
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string town, file;
+    int year, population = 0;
+
+    cout << "What is the name of the town? ";
+    getline(cin, town);
+    cout << "What is the name of the data file (Include '.txt' in answer)? ";
+    getline(cin, file);
+
+    ifstream inputFile(file);
+
+    if (inputFile)
+    {
+        cout << endl << town << " Population Growth\n";
+        cout << "Each * represents 1,000 people\n" << endl;
+
+        while (inputFile >> year)
+        {
+            cout << year << " ";
+
+            inputFile >> population;
+            population /= 1000;
+            while (population > 0)
+            {
+                cout << "*";
+                population -= 1;
+            }
+
+            cout << endl;
+        }
+        inputFile.close();
+    }
+    else
+        cout << "\nError opening the file.\n";
+
     return 0;
 }
