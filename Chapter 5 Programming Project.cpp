aba1202442bcf3e2a1ab87296a4b5b6cbe6ee374
jjw86297 <jjw86297@email.vccs.edu>
2024-10-24 20:05:41 -0400

    First (and Last?) Commit

--- Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project.cpp
@@ -1,13 +1,33 @@
 //Jacob Walrath
 //10/24/2024
 //Chapter 5 Programming Project
-//master
+//Student_Lineup
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string name;
+    int students = 0;
+
+    ifstream inputFile("LineUp.txt");
+
+    while (inputFile >> name)
+    {
+        students += 1;
+        if (students == 1)
+        {
+            cout << name << " is at the front of the line.\n";
+        }
+    }
+    cout << name << " is at the back of the line.\n" << endl;
+
+    cout << "There are " << students << " students in the class.\n";
+    
+    inputFile.close();
     return 0;
 }
