21e495d1597937e83965ccaa7ed3feb5270026c1
jjw86297 <jjw86297@email.vccs.edu>
2024-10-24 22:53:01 -0400

    First and Final Commit

--- Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project.cpp
@@ -9,5 +9,27 @@ using namespace std;
 
 int main()
 {
+    char plus = '+';
+
+    cout << "Pattern A\n";
+    for (int count = 0; count < 10; count++)
+    {
+        for (int up = 0; up < (1 + count); up++)
+        {
+            cout << plus;
+        }
+        cout << endl;
+    }
+
+    cout << "\nPattern B\n";
+    for (int count = 0; count < 10; count++)
+    {
+        for (int up = 0; up < (10 - count); up++)
+        {
+            cout << plus;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
